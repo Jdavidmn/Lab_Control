ebe9199fa3baea629bb59ddd3b8639591d9d0b02
David Monge <j.davidmn@hotmail.com>
2023-03-15 14:43:54 -0600

    modelo estocastico de pamh

---
+++ PAMH/modelo.m
@@ -0,0 +1,22 @@
+s = tf('s');
+
+matriz = csvread('PAMH_3Set.CSV', 1, 0);
+
+tiempo = matriz(74:2022,1);
+angulo = matriz(74:2022,2);
+estimulo = matriz(74:2022,3);
+
+tf3 = tf([5.3807], [1 0.1984 15.6570]);
+
+gmax = 1.14;
+gmax1 = 1.031;
+T = 1.62;
+
+delta = log(1.14/1.031);
+
+xi = delta/(sqrt((2*pi)^2+delta^2));
+k = gmax/1.7;
+
+w = 2*pi/(T*sqrt(1-xi^2));
+
+model = k*w^2/(s^2+2*xi*w*s+w^2);
