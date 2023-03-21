0ded7427a3df2a9e8435c937c82771832c9b6549
David Monge <j.davidmn@hotmail.com>
2023-03-21 07:48:30 -0600

    modelo de pamh

--- PAMH/modelo.m
+++ PAMH/modelo.m
@@ -6,7 +6,9 @@ tiempo = matriz(74:2022,1);
 angulo = matriz(74:2022,2);
 estimulo = matriz(74:2022,3);
 
-tf3 = tf([5.3807], [1 0.1984 15.6570]);
+tf3 = tf([5.3807], [1 0.1984 15.6570]);             % la que da el system identification
+
+%systemIdentification('modelo.sid');                % abre el system identification, da un porcentaje de 86%
 
 gmax = 1.14;
 gmax1 = 1.031;
@@ -19,4 +21,4 @@ k = gmax/1.7;
 
 w = 2*pi/(T*sqrt(1-xi^2));
 
-model = k*w^2/(s^2+2*xi*w*s+w^2);
+model = k*w^2/(s^2+2*xi*w*s+w^2);           % la que da el modelo a pata
