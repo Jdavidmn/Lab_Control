2de8eff48ac0b90b2831b27bea604af3c9eb3d56
user9000cr <dherrerac00@gmail.com>
2023-03-25 04:00:16 -0600

    Listo control IMC

--- PAMH/control.m
+++ PAMH/control.m
@@ -12,3 +12,29 @@ tf3 = tf([5.3807], [1 0.1984 15.6570]);
 
 % Modelo a partir de las ecuaciones
 tf_math = tf([5.176], [1 0.1241 15.05]);
+
+%----------------------------
+%-      DISEÑO DEL IMC      -
+%----------------------------
+
+% Se usa sisotool para diseñar el control
+%sisotool(tf3)
+
+tf_ctrl_imc = tf([0.7434 0.7434*0.1984 0.7434*15.66], [1 4 0]);
+
+syms KP KI KD N S;
+
+[num, den]=tfdata(C,'v');
+PID=KP+KI/S+(KD*N*S)/(S+N);
+collect(PID);
+N=den(2);
+KI=num(3)/N;
+KP=(num(2)-KI)/N;
+KD=(num(1)-KP)/N;
+ 
+% Se grafica la respuesta del step con el control
+figure();
+step(feedback(tf_ctrl_imc*tf3,1));
+
+
+ 
