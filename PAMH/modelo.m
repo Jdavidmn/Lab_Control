d1480e8f770c367a92937807d72d6a65163363dc
David <j.davidmn@hotmail.com>
2023-04-14 16:33:50 -0600

    Constantes comentadas

--- PAMH/modelo.m
+++ PAMH/modelo.m
@@ -1,5 +1,6 @@
-syms KP KD KI N s S
+syms KP KD KI N S
 
+s = tf("s");
 matriz = csvread('PAMH_3Set.CSV', 1, 0);
 
 tiempo = matriz(74:2022,1);
@@ -25,7 +26,9 @@ model = k*w^2/(s^2+2*xi*w*s+w^2);           % la que da el modelo a pata
 
 % step(feedback(C*tf3,1));
 
-% El control C hay que importarlo del archivo .mat (sisotool)
+% El control C hay que importarlo del archivo PID.mat (sisotool)
+% es solo para el PID, no afecta para el IPD
+% sisotool("PID.mat")
 
 %------------------------------------------------------------------------
 
@@ -54,24 +57,57 @@ Ks = acker(As, Bs, Ps);
 
 % metodo de lqr
 
-Q = [0.5 0 0; 0 5 0; 0 0 10];
+%Q = [0.5 0 0; 0 5 0; 0 0 10];   % opcion 1
 
 % Opciones de Q
-% Q = [0.5 0 0; 0 5 0; 0 0 10];
-% Q = [1 0 0; 0 10 0; 0 0 10];
-% Q = [0.005 0 0; 0 5 0;  0 0 10];
+%Q = [0.5 0 0; 0 10 0; 0 0 20];     % opcion 2
+%Q = [1 0 0; 0 10 0; 0 0 10];      % opcion 3
+Q = [0.005 0 0; 0 5 0;  0 0 10];  % opcion 4
 
 R = 1;
 
-Kss = lqr(As, Bs, Q, R)
+Kss = lqr(As, Bs, Q, R);
+
+%--------------------------Constantes resultantes-------------------------
+
+% opcion 1:
+% Ki = 3.1623
+% Kd = 2.3601
+% Kp = 2.0021
+% 
+% opcion 2:
+% Ki = 4.4721
+% Kd = 3.3157
+% Kp = 3.3314
+% 
+% opcion 3:
+% Ki = 3.1623
+% Kd = 3.2752
+% Kp = 2.6051
+% 
+% opcion 4:
+% Ki = 3.1623
+% Kd = 2.3560
+% Kp = 1.9487
+
+% El N, se usa 0.5 en la forma:
+%    s
+%  ------
+%   Ns+1
+
+% Ó N = 2 en la forma
+%    Ns
+%  ------
+%   s+N
+%-------------------------------------------------------------------------
 
 % ubicacion de los polos por lqr
 
 polos = eig(As - Bs*Kss);
 
-Ki = -Kss(3);
-Kd = Kss(2);
-Kp = Kss(1);
+Ki = -Kss(3)
+Kd = Kss(2)
+Kp = Kss(1)
 
 tf1 = tf([1 0],[0.5 1]);
 
@@ -81,11 +117,13 @@ tf1 = tf([1 0],[0.5 1]);
 % sisotool('rlocus', tf3);
 
 %Control PID por IMC
-%[num, den]=tfdata(C,'v');
-%PID=KP+KI/S+(KD*N*S)/(S+N);
-%collect(PID);
-%N=den(2);
-%KI=num(3)/N;
-%KP=(num(2)-KI)/N;
-%KD=(num(1)-KP)/N;
+[num, den]=tfdata(C,'v');
+PID=KP+KI/S+(KD*N*S)/(S+N);
+collect(PID);
+N=den(2);
+% N = 2;
+KI=num(3)/N;
+KP=(num(2)-KI)/N;
+KD=(num(1)-KP)/N;
 
+PID_IMC=zpk(KP+KI/s+(KD*N*s)/(s+N));
